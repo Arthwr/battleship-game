f7d056020a073b41daed6561a1e624d7a42e7ce3
arthur <artyr007@gmail.com>
2024-07-07 01:25:05 +0500

    refactor(grid): add label rows and columns in grid component creation
    -Add respective css styles to match grid positioning

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,16 +4,41 @@ export default function grid(players) {
   players.forEach((player) => {
     const { rowCount, columnCount } = player.gameBoard.getDimensions();
 
-    let playerGridHTML = "";
+    let xAxisLabelHTML = "";
+    for (let col = 1; col <= columnCount; col++) {
+      const label = String.fromCharCode(64 + col);
+      xAxisLabelHTML += `<div class="label-cell">${label}</div>`;
+    }
+
+    let yAxisLabelHTML = "";
+    for (let row = 1; row <= rowCount; row++) {
+      yAxisLabelHTML += `<div class="label-cell">${row}</div>`;
+    }
+
+    let playerGridHTML = `
+      <div class="gameboard">
+        <div class="label-row">
+          <div class="label-cell"></div>
+        ${xAxisLabelHTML}
+        </div>
+        <div class="grid-wrap">
+          <div class="label-column">${yAxisLabelHTML}</div>
+          <div id="${player.name}" class="grid">
+    `;
+
     for (let row = 1; row <= rowCount; row++) {
       let rowHTML = '<div class="row">';
       for (let col = 1; col <= columnCount; col++) {
-        rowHTML += `<div class="cell" data-row="${row}" data-col="${col}"></div>`;
+        rowHTML += `<div class="game-cell" data-row="${row}" data-col="${col}"></div>`;
       }
       rowHTML += "</div>";
       playerGridHTML += rowHTML;
     }
-    playerGridHTML = `<div id="${player.name}" class="grid">${playerGridHTML}</div>`;
+    playerGridHTML += `
+        </div>
+      </div>
+    </div>
+    `;
     gridsHTML += playerGridHTML;
   });
 
