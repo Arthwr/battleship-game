c0575bf5353deb9dd23802cc98d48bbe09c92ded
arthur <artyr007@gmail.com>
2024-07-06 21:46:03 +0500

    feat(gameController): initialization of new game through gameController in index.js as entry point
    -Add players dependency to GameController class
    -Initialize players array and call setupNewGame method in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import GameController from "./modules/controller/GameController";
+import Player from "./modules/models/Player";
+import Gameboard from "./modules/models/Gameboard";
+import View from "./modules/view/View";
 import "./styles/index.css";
+
+const players = [
+  new Player("User", new Gameboard()),
+  new Player("Computer", new Gameboard()),
+];
+
+const game = new GameController(players, new View());
+game.setupNewGame();
