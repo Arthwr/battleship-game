a06d2af679f1af01114824ff5d9b9cc6824dc45a
arthur <artyr007@gmail.com>
2024-07-09 04:43:08 +0500

    feat(GameController): add auto switch between user and computer player and prevent interaction on not currently active gameboard
    -Add switchPlayer method and currentPlayer variable to hold the value in class constructor
    -Add guard clause for already attacked cell status to prevent wasting player turn in handleBoarClick method

--- src/modules/controller/GameController.js
+++ src/modules/controller/GameController.js
@@ -1,20 +1,33 @@
 export default class GameController {
   constructor(players, view) {
     this.players = players;
+    this.currentPlayer = this.players[1];
     this.view = view;
   }
 
+  switchPlayer() {
+    this.currentPlayer =
+      this.currentPlayer === this.players[0]
+        ? (this.currentPlayer = this.players[1])
+        : (this.currentPlayer = this.players[0]);
+  }
+
   handleBoardClick(player, coordinates) {
-    const gameBoard = player.gameBoard;
-    const attackStatus = gameBoard.receiveAttack(coordinates);
-    this.view.updateCell(player.name, coordinates, attackStatus);
+    if (this.currentPlayer === player) {
+      const gameBoard = player.gameBoard;
+      const attackStatus = gameBoard.receiveAttack(coordinates);
+
+      if (attackStatus === "already_attacked") return;
+      this.view.updateCell(player.name, coordinates, attackStatus);
+      this.switchPlayer();
+    }
   }
 
   attachListeners() {
     const gridList = this.view.gameGrid;
     gridList.forEach((grid, index) => {
       grid.addEventListener("click", (event) => {
-        const cell = event.target.closest(".game-cell")
+        const cell = event.target.closest(".game-cell");
         if (cell) {
           const player = this.players[index];
           const row = cell.dataset.row;
