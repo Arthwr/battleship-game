788921b6a383c82311916166ac3162625c60f166
arthur <artyr007@gmail.com>
2024-07-09 03:45:51 +0500

    feat(gameController): add handleBoardClick and attachListeners methods for DOM interaction

--- src/modules/controller/GameController.js
+++ src/modules/controller/GameController.js
@@ -3,8 +3,30 @@ export default class GameController {
     this.players = players;
     this.view = view;
   }
-  
+
+  handleBoardClick(player, coordinates) {
+    const gameBoard = player.gameBoard;
+    const attackStatus = gameBoard.receiveAttack(coordinates);
+    this.view.updateCell(player.name, coordinates, attackStatus);
+  }
+
+  attachListeners() {
+    const gridList = this.view.gameGrid;
+    gridList.forEach((grid, index) => {
+      grid.addEventListener("click", (event) => {
+        const cell = event.target.closest(".game-cell")
+        if (cell) {
+          const player = this.players[index];
+          const row = cell.dataset.row;
+          const col = cell.dataset.col;
+          this.handleBoardClick(player, [row, col]);
+        }
+      });
+    });
+  }
+
   setupNewGame() {
     this.view.updateView(this.players);
+    this.attachListeners();
   }
 }
