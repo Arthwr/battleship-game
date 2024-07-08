ef8d498b2cf5fb72b902923ed5d469978cd1a29d
arthur <artyr007@gmail.com>
2024-07-08 20:32:04 +0500

    feat(view): add renderShips and renderSingleShip methods
    -Integrate method to render ships on updateView function to render placed ships on grid for each player
    -Add several ship placements to visually test render logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import GameController from "./modules/controller/GameController";
 import Player from "./modules/models/Player";
 import Gameboard from "./modules/models/Gameboard";
+import Ship from "./modules/models/Ship";
 import View from "./modules/view/View";
 import "./styles/index.css";
 
@@ -8,6 +9,24 @@ const players = [
   new Player("User", new Gameboard()),
   new Player("Computer", new Gameboard()),
 ];
+
+// Place 4 ships with length 1
+players[0].gameBoard.place(new Ship(1), [9, 9]);
+players[0].gameBoard.place(new Ship(1), [9, 7]);
+players[0].gameBoard.place(new Ship(1), [6, 8]);
+players[0].gameBoard.place(new Ship(1), [4, 9]);
+
+// Place 3 ships with length 2
+players[0].gameBoard.place(new Ship(2), [[10, 1],[10,2]]);
+players[0].gameBoard.place(new Ship(2), [[8, 1],[7,1]]);
+players[0].gameBoard.place(new Ship(2), [[2, 8],[2,9]]);
+
+// Place 2 ships with length 3
+players[0].gameBoard.place(new Ship(2), [[7, 3],[7,4], [7,5]]);
+players[0].gameBoard.place(new Ship(2), [[4, 3],[4,4], [4,5]]);
+
+// Place 1 ships with length 4
+players[0].gameBoard.place(new Ship(2), [[2, 2],[2,3], [2,4], [2,5]]);
 
 const game = new GameController(players, new View());
 game.setupNewGame();
--- src/modules/view/View.js
+++ src/modules/view/View.js
@@ -5,13 +5,36 @@ export default class View {
     this.gameContainer = document.getElementById("game-container");
   }
 
-  updateView(players) {
-    this.gameContainer.innerHTML = "";
-    this.renderGrid(players)
-  }
-
   renderGrid(players) {
     const combinedGridHTML = grid(players);
     this.gameContainer.innerHTML = combinedGridHTML;
+  }
+
+  renderSingleShip(row, col, boardId) {
+    const cellSelector = `.row[data-row="${row}"] .game-cell[data-col="${col}"]`;
+    const gameBoard = document.getElementById(boardId);
+    const cell = gameBoard.querySelector(cellSelector);
+    cell.classList.add("ship");
+  }
+
+  renderShips(player) {
+    const gameBoard = player.grid();
+    if (gameBoard !== null) {
+      gameBoard.forEach((row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+          if (cell && typeof cell === "object") {
+            const rowToRender = rowIndex + 1;
+            const colToRender = colIndex + 1;
+            this.renderSingleShip(rowToRender, colToRender, player.name);
+          }
+        });
+      });
+    }
+  }
+
+  updateView(players) {
+    this.gameContainer.innerHTML = "";
+    this.renderGrid(players);
+    players.forEach((player) => this.renderShips(player));
   }
 }
