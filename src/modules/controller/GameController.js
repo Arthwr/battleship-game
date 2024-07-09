94f4ed12d90ba416c37e8397a6aca4bc69228a90
arthur <artyr007@gmail.com>
2024-07-09 22:04:29 +0500

    fix(GameController): handleBoardClick to properly reflect current player as to allow action only if its not current player's turn
    -Simplify ternary operator logic in switchPlayer method
    -Rename variable name from gridList to gameGrids

--- src/modules/controller/GameController.js
+++ src/modules/controller/GameController.js
@@ -1,31 +1,32 @@
 export default class GameController {
   constructor(players, view) {
     this.players = players;
-    this.currentPlayer = this.players[1];
+    this.currentPlayer = this.players[0];
     this.view = view;
   }
 
   switchPlayer() {
     this.currentPlayer =
       this.currentPlayer === this.players[0]
-        ? (this.currentPlayer = this.players[1])
-        : (this.currentPlayer = this.players[0]);
+        ? this.players[1]
+        : this.players[0];
   }
 
   handleBoardClick(player, coordinates) {
-    if (this.currentPlayer === player) {
+    if (this.currentPlayer !== player) {
       const gameBoard = player.gameBoard;
       const attackStatus = gameBoard.receiveAttack(coordinates);
 
       if (attackStatus === "already_attacked") return;
+
       this.view.updateCell(player.name, coordinates, attackStatus);
       this.switchPlayer();
     }
   }
 
   attachListeners() {
-    const gridList = this.view.gameGrid;
-    gridList.forEach((grid, index) => {
+    const gameGrids = this.view.gameGrid;
+    gameGrids.forEach((grid, index) => {
       grid.addEventListener("click", (event) => {
         const cell = event.target.closest(".game-cell");
         if (cell) {
