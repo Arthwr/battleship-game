ebfadbc0c34aadfc4f4b12b1a9ddf322d6df6961
arthur <artyr007@gmail.com>
2024-07-09 23:53:58 +0500

    feat(GameController): add basic computer player logic in GameController class
    -add triggerComputerTurn method to trigger computerTurn method with 1 second delay
    -add computerTurn to randomly choose row and col for his next attack and call handleBoardClick method for that choosen cell
     Also keeps record of all uniue attacks done by computer in separate computerAttacks variable as a Set.

--- src/modules/controller/GameController.js
+++ src/modules/controller/GameController.js
@@ -3,6 +3,7 @@ export default class GameController {
     this.players = players;
     this.currentPlayer = this.players[0];
     this.view = view;
+    this.computerAttacks = new Set();
   }
 
   switchPlayer() {
@@ -21,7 +22,31 @@ export default class GameController {
 
       this.view.updateCell(player.name, coordinates, attackStatus);
       this.switchPlayer();
+      this.triggerComputerTurn();
     }
+  }
+
+  triggerComputerTurn() {
+    if (this.currentPlayer.name === "Computer") {
+      setTimeout(() => {
+        this.computerTurn();
+      }, 1000);
+    }
+  }
+
+  computerTurn() {
+    let attackOnRow, attackOnCol;
+    const userPlayer = this.players[0];
+    const { rowCount, columnCount } = userPlayer.gameBoard.getDimensions();
+
+    do {
+      attackOnRow = Math.floor(Math.random() * (rowCount - 1)) + 1;
+      attackOnCol = Math.floor(Math.random() * (columnCount - 1)) + 1;
+    } while (this.computerAttacks.has(`${attackOnRow}, ${attackOnCol}`));
+
+    this.computerAttacks.add(`${attackOnRow}, ${attackOnCol}`);
+    console.log(this.computerAttacks)
+    this.handleBoardClick(this.players[0], [attackOnRow, attackOnCol]);
   }
 
   attachListeners() {
