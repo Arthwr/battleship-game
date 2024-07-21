c800c43b85fb36751e4587a7f84426239f506ea0
arthur <artyr007@gmail.com>
2024-07-21 21:45:09 +0500

    feat(introElement): add simple form req validation for human name inputs, values for select, return form to be used in other modules

--- src/components/introElement.js
+++ src/components/introElement.js
@@ -3,33 +3,33 @@ export default function introElement() {
   intro.id = "menu";
 
   intro.innerHTML = `
-    <form>
+    <form id="game-form">
         <h2>Choose Players</h2>
         <div>
             <div class="menu-col">
                 <div>
-                    <label for="player1-type">Player:</label>
+                    <label for="player1-type">Player 1:</label>
                     <select id="player1-type" name="player1-type">
-                        <option>Human</options>
-                        <option>Computer</options>
+                        <option value="human">Human</options>
+                        <option value="computer">Computer</options>
                     </select>
                 </div>
                 <div>
                     <label for="player1-name">Name:</label>
-                    <input id="player1-name" name="player1-name" type="text"></input>
+                    <input id="player1-name" name="player1-name" type="text" required></input>
                 </div>
             </div>
             <div class="menu-col">
                 <div>
-                    <label for="player2-type">Player:</label>
+                    <label for="player2-type">Player 2:</label>
                     <select id="player2-type" name="player2-type">
-                        <option>Human</options>
-                        <option>Computer</options>
+                        <option value="human">Human</options>
+                        <option selected value="computer">Computer</options>
                     </select>
                 </div>
                 <div>
                     <label for="player2-name">Name:</label>
-                    <input id="player2-name" name="player2-name" type="text"></input>
+                    <input id="player2-name" name="player2-name" type="text" required disabled></input>
                 </div>
             </div>
         </div>
@@ -37,5 +37,6 @@ export default function introElement() {
     </form>
     `;
 
-  return intro;
+  const form = intro.querySelector("#game-form");
+  return { intro, form };
 }
