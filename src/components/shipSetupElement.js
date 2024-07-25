9969e49f36c05b731a92f002c2bcfeff2ab0d40e
arthur <artyr007@gmail.com>
2024-07-25 21:38:38 +0500

    component(shipSetupElement): add game-object wrappers around ships, dynamically assign width and draggable attribute
    -Add necessary css styles to new elements

--- src/components/shipSetupElement.js
+++ src/components/shipSetupElement.js
@@ -7,25 +7,45 @@ export default function shipSetupElement() {
     <div>
         <div>Prowlers:</div>
         <div class="ships-wrapper">
-            <div class="game-object draggable" data-length="1"></div>
-            <div class="game-object draggable" data-length="1"></div>
-            <div class="game-object draggable" data-length="1"></div>
-            <div class="game-object draggable" data-length="1"></div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="1"></div>
+            </div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="1"></div>
+            </div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="1"></div>
+            </div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="1"></div>
+            </div>
         </div>
         <div>Sea Serpent:</div>
         <div class="ships-wrapper">
-            <div class="game-object draggable" data-length="2"></div>
-            <div class="game-object draggable" data-length="2"></div>
-            <div class="game-object draggable" data-length="2"></div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="2"></div>
+            </div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="2"></div>
+            </div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="2"></div>
+            </div>
         </div>
         <div>Tempest Titan:</div>
         <div class="ships-wrapper">
-            <div class="game-object draggable" data-length="3"></div>
-            <div class="game-object draggable" data-length="3"></div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="3"></div>
+            </div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="3"></div>
+            </div>
         </div>
         <div>Leaviathan Lord:</div>
         <div class="ships-wrapper">
-            <div class="game-object draggable" data-length="4"></div>
+            <div class="object-wrapper">
+              <div class="game-object" data-length="4"></div>
+            </div>
         </div>
     </div>  
   `;
@@ -36,11 +56,15 @@ export default function shipSetupElement() {
       const length = ship.getAttribute("data-length");
       if (length) {
         for (let i = length; i > 0; i--) {
-            const shipCell = document.createElement("div");
-            shipCell.classList.add("ship");
-            ship.appendChild(shipCell);
+          const shipCell = document.createElement("div");
+          shipCell.classList.add("ship");
+          ship.appendChild(shipCell);
         }
       }
+
+      const objWrapper = ship.closest(".object-wrapper");
+      objWrapper.style.width = `${length * 2}rem`;
+      objWrapper.setAttribute("draggable", "true");
     });
   }, 0);
 
