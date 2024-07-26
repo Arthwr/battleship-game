5e784f979d1559fada7fa9c0afb9d5dba08078ae
arthur <artyr007@gmail.com>
2024-07-26 19:27:42 +0500

    chore(components): change identifier of game object from dataset.name to element id
    -Remove setting of draggable attribute to true in async component fnc

--- src/components/shipSetupElement.js
+++ src/components/shipSetupElement.js
@@ -7,43 +7,43 @@ export default function shipSetupElement() {
     <div>
         <div>Prowlers:</div>
         <div class="ships-wrapper">
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="prowler1">
               <div class="game-object" data-length="1"></div>
             </div>
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="prowler2">
               <div class="game-object" data-length="1"></div>
             </div>
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="prowler3">
               <div class="game-object" data-length="1"></div>
             </div>
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="prowler4">
               <div class="game-object" data-length="1"></div>
             </div>
         </div>
         <div>Sea Serpent:</div>
         <div class="ships-wrapper">
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="serpent1">
               <div class="game-object" data-length="2"></div>
             </div>
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="serpent2">
               <div class="game-object" data-length="2"></div>
             </div>
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="serpent3">
               <div class="game-object" data-length="2"></div>
             </div>
         </div>
         <div>Tempest Titan:</div>
         <div class="ships-wrapper">
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="titan1">
               <div class="game-object" data-length="3"></div>
             </div>
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="titan2">
               <div class="game-object" data-length="3"></div>
             </div>
         </div>
         <div>Leaviathan Lord:</div>
         <div class="ships-wrapper">
-            <div class="object-wrapper">
+            <div class="object-wrapper" id="lord1">
               <div class="game-object" data-length="4"></div>
             </div>
         </div>
@@ -64,7 +64,6 @@ export default function shipSetupElement() {
 
       const objWrapper = ship.closest(".object-wrapper");
       objWrapper.style.width = `${length * 2}rem`;
-      objWrapper.setAttribute("draggable", "true");
     });
   }, 0);
 
