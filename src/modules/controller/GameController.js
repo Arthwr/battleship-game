34d8c4447f3f93ad0e561488b577ae3d51d298cf
arthur <artyr007@gmail.com>
2024-07-30 21:20:36 +0500

    chore(refactor): remove inline arrow functions clickHandlers and bind their this context through bind() fnc

--- src/modules/controller/GameController.js
+++ src/modules/controller/GameController.js
@@ -2,10 +2,14 @@ export default class GameController {
   constructor(game, view) {
     this.game = game;
     this.view = view;
+
+    this.handleGridClick = this.handleGridClick.bind(this);
+    this.handleFormStartClick = this.handleFormStartClick.bind(this);
+    this.handleMove = this.handleMove.bind(this);
   }
 
   init() {
-    this.view.showPlayersForm(this.#handleFormStartClick);
+    this.view.showPlayersForm(this.handleFormStartClick);
   }
 
   setupPlayerFleet() {
@@ -20,7 +24,25 @@ export default class GameController {
     this.#attachGameBoardListeners();
   }
 
-  #handleMove = (coordinates, event = null) => {
+  handleGridClick(event) {
+    // Prevent humanPlayer click on gameboard during computer turn
+    const currentPlayer = this.game.getCurrentPlayer();
+    if (currentPlayer.name === "computer") return;
+
+    const cell = event.target.closest(".game-cell");
+    if (cell) {
+      const coordinates = [cell.dataset.row, cell.dataset.col];
+      this.handleMove(coordinates, event);
+    }
+  }
+
+  handleFormStartClick(form) {
+    const playersData = form.querySelectorAll(".menu-col");
+    this.game.setupPlayers(playersData);
+    this.setupPlayerFleet();
+  }
+
+  handleMove(coordinates, event = null) {
     const currentPlayer = this.game.getCurrentPlayer();
 
     // Check if the humanPlayer is trying to attack their own board
@@ -41,8 +63,8 @@ export default class GameController {
     }
 
     this.#handlePlayerLabelUpdate();
-    this.game.endTurn(status, this.#handleMove);
-  };
+    this.game.endTurn(status, this.handleMove);
+  }
 
   #handleGameEnd(winner) {
     this.#removeGameBoardListeners();
@@ -54,33 +76,15 @@ export default class GameController {
     this.view.updatePlayerLabel(currentPlayer);
   }
 
-  #handleGridClick = (event) => {
-    // Prevent humanPlayer click on gameboard during computer turn
-    const currentPlayer = this.game.getCurrentPlayer();
-    if (currentPlayer.name === "computer") return;
-
-    const cell = event.target.closest(".game-cell");
-    if (cell) {
-      const coordinates = [cell.dataset.row, cell.dataset.col];
-      this.handleMove(coordinates, event);
-    }
-  };
-
-  #handleFormStartClick = (form) => {
-    const playersData = form.querySelectorAll(".menu-col");
-    this.game.setupPlayers(playersData);
-    this.setupPlayerFleet();
-  };
-
   #attachGameBoardListeners() {
     this.view.gameGrid.forEach((grid) => {
-      grid.addEventListener("click", this.#handleGridClick);
+      grid.addEventListener("click", this.handleGridClick);
     });
   }
 
   #removeGameBoardListeners() {
     this.view.gameGrid.forEach((grid) => {
-      grid.removeEventListener("click", this.#handleGridClick);
+      grid.removeEventListener("click", this.handleGridClick);
     });
   }
 }
