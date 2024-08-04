c017a181c7f26f7ebf229c7fa38335f441f2f01d
arthur <artyr007@gmail.com>
2024-08-04 20:41:46 +0500

    tests(Player): fix unit tests due to the change in method names

--- tests/models/Player.test.js
+++ tests/models/Player.test.js
@@ -29,10 +29,10 @@ describe("Player Model", () => {
     });
   });
 
-  test("should correctly return move set for computer chooseMove method", () => {
+  test("should correctly return move set for computer chooseRandomMove method", () => {
     const opponentBoard = new Gameboard(10, 10);
     const computerPlayer = new ComputerPlayer(new Gameboard(10, 10));
-    const move = computerPlayer.chooseMove(opponentBoard);
+    const move = computerPlayer.chooseRandomMove(opponentBoard);
 
     expect(move).toHaveLength(2);
     expect(typeof move[0]).toBe("number");
@@ -43,19 +43,19 @@ describe("Player Model", () => {
     expect(move[1]).toBeLessThan(opponentBoard.getDimensions().columnCount);
   });
 
-  test("chooseMove should avoid repeating moves", () => {
+  test("chooseRandomMove should avoid repeating moves", () => {
     const gameBoard = new Gameboard(3, 3);
     const computerPlayer = new ComputerPlayer(gameBoard);
     const initialAttackHistorySize = computerPlayer.attackHistory.size;
 
-    const move1 = computerPlayer.chooseMove(gameBoard);
+    const move1 = computerPlayer.chooseRandomMove(gameBoard);
     expect(computerPlayer.attackHistory.size).toBe(
       initialAttackHistorySize + 1
     );
 
     jest.spyOn(global.Math, "random").mockReturnValueOnce(0);
     jest.spyOn(global.Math, "random").mockReturnValueOnce(0);
-    const move2 = computerPlayer.chooseMove(gameBoard);
+    const move2 = computerPlayer.chooseRandomMove(gameBoard);
 
     expect(move2).not.toEqual(move1);
     expect(computerPlayer.attackHistory.size).toBe(
