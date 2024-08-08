6540492bad04cb7f8deda58fe6dcc51a7b90b1aa
arthur <artyr007@gmail.com>
2024-08-08 22:36:16 +0500

    chore(constants): add constants.js file to keep constants in a single file
    -To hold information about columns and rows in gameboard
    -To hold information about ship types and amount of them

---
+++ src/modules/data/constants.js
@@ -0,0 +1,11 @@
+export const GameBoardConfig = Object.freeze({
+  MAX_ROWS: 10,
+  MAX_COLUMNS: 10,
+});
+
+export const SHIPS = Object.freeze([
+  { name: "prowler", length: 1, count: 4 },
+  { name: "serpent", length: 2, count: 3 },
+  { name: "titan", length: 3, count: 2 },
+  { name: "lord", length: 4, count: 1 },
+]);
