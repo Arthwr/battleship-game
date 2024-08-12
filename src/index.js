cd7b7518a2f4084b8748000ba783e57563191e7e
arthur <artyr007@gmail.com>
2024-08-12 22:08:06 +0500

    chore(index.js): remove comments

--- src/index.js
+++ src/index.js
@@ -5,9 +5,3 @@ import "./styles/index.css";
 
 const game = new GameController(new Game(), new View());
 game.init();
-
-// To do:
-// 1. prevent ship collision from switching horizontal to vertical and vice versa;
-// 2. prevent ship placement in close proximity of other ships (minimum 1 cell distance between each ship);
-// 3. fix a bug where you can't place ship on a new position if at least 1 cell was occupied by that ship before;
-// 4. show player id in result game message
